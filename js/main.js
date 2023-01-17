3ecc732ccfa274ac55aebd0da14801c79b3e49d4
Les Carbonaro <lcarbonaro@gmail.com>
2023-01-16 22:04:41 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+document.querySelector('#clickMe').addEventListener('click', makeReq);
+
+document.querySelector('#btnCoinFlip').addEventListener('click', makeCoinFlipReq)
+
+
+async function makeCoinFlipReq() {  
+  const res = await fetch(`/coin`);
+  const data = await res.json();
+  console.log(data);
+  document.querySelector("#h2CoinFlipResult").textContent = data.flip;
+}
+
+async function makeReq(){
+
+  const userName = document.querySelector("#userName").value;
+  const res = await fetch(`/api?student=${userName}`)
+  const data = await res.json()
+
+  console.log(data);
+  document.querySelector("#personName").textContent = data.name
+  document.querySelector("#personStatus").textContent = data.status
+  document.querySelector("#personOccupation").textContent = data.currentOccupation
+}
